52fad1cb703c7e698ddc4c7a45a0e410edc2a1a9
white.privat@gmail.com <white.privat@gmail.com>
2024-01-06 16:30:49 +0100

    added some basic code and tested forecast api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import FetchForecast from './components/api/FetchForecast';
 function App() {
+  FetchForecast()
   return (
     <div className="App">
 
