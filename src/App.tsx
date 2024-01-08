567f2530c16f813d1b22abf1e90e29ab4bcb3f9b
white <white.privat@gmail.com>
2024-01-08 21:50:15 +0100

    done some testing to the tremor framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import forecast from './components/api/forecast';
+
 function App() {
   forecast()
   return (
---
+++ src/components/tremor/currentCharts.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+export default (params:string) => {
+    console.log(params)
+    return (
+        <div></div>
+    )
+}
