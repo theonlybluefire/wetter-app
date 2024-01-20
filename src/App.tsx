5575981193e6101b5b6f3673621fad1bb47aae60
theonlybluefire <white.privat@gmail.com>
2024-01-20 19:42:45 +0100

    added the fetch function
    function works but it somehow triggers a rerender, even if there is a effectHook

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,9 @@ import Chart from './components/tremor/Chart';
 import geocoding from './components/api/geocoding';
 function App() {
   useEffect(() => {
-    (async () => {
-        try {
-            const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${search}&count=20&language=de&format=json`);
-            const data = await response.json();
-            console.log(data)
-        } catch (error) {
-            console.error(error);
-        }
-    })();
-}, []);
+    geocoding('Berlin')
+  },[])
+
   forecast()
   return (
     <div>
--- src/components/api/geocoding.tsx
+++ src/components/api/geocoding.tsx
@@ -1,5 +1,12 @@
-
-import React, { useEffect } from "react"
+import React from "react";
 export default async (search) => {
-    
+    //get data from the api
+    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${search}&count=10&language=de&format=json`, {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    },
+})
+   .then(response => response.json())
+   .then(response => console.log(JSON.stringify(response)))
 }
