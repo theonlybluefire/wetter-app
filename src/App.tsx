df7476cf7a30bcd381f50aea0598d1b3f9f9dade
theonlybluefire <white.privat@gmail.com>
2024-02-14 22:28:36 +0100

    implmented a query System to process the data
    some errors, need to log the data I am getting from QueryAPI in sidePanel and see if there is any data

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import ProgressCircle from './components/tremor/ProgressCircle';
 import Chart from './components/tremor/Chart';
 import QueryApi from './components/api/QueryApi';
 import Geocoding from './components/api/Geocoding';
+import SidePanel from './components/ui/sidePanel';
 function App() {
   return (
     <div>
-      <QueryApi/>
-      <Geocoding location='Wain'/>
+      <SidePanel/>
     </div>
   );
 }
--- src/components/ui/sidePanel.tsx
+++ src/components/ui/sidePanel.tsx
@@ -1,8 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
+import { useQuery } from "@tanstack/react-query"
+import QueryApi from "../api/QueryApi"
 export default () => {
-    
-    return (
-        <div></div>
-    ) 
+    const [weather, setWheater] = useState<string>()
+    const data = QueryApi();
+    if(!data.isLoading) {
+        if(data.current.rain > 0) {
+            setWheater('Rain')
+        }
+        else if(data.current.showers > 0) {
+            setWheater('Shower')
+        }
+        else if(data.current.snowfall > 0) {
+            setWheater('Snow')
+        }
+        return (
+            <div className="background">
+                <div className="weatherDisplay">
+                    {weather}
+                </div>
+                <div className="temperatureDisplay">
+                    {Math.round(data.current.apparentTemperature)}
+                </div>
+            </div>
+        ) 
+    }
+    if(data.isLoading) {
+        return <div>Loading....</div>
+    }
 
 }
