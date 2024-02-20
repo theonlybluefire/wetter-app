e4894c238f971402db708d85303b4e3eb0a570d2
theonlybluefire <white.privat@gmail.com>
2024-02-20 21:11:23 +0100

    done the geocoding table

--- src/components/api/Geocoding.tsx
+++ src/components/api/Geocoding.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react"
 import { useQuery } from "@tanstack/react-query"
+import {  Table,  TableBody,  TableCell,  TableHead,  TableHeaderCell,  TableRow,} from '@tremor/react';
+
+
+
+
+
+
+
 export default (location) => {
     const resultName = null
     console.log(JSON.stringify(location))
@@ -16,15 +24,57 @@ export default (location) => {
     if (geocodingQuery.error) {
         return <div>geocodingQuery.error</div>
         }
-    for(let i = 0;i<geocodingQuery.data.results;i++) {
-        resultName.push(geocodingQuery.data.results)
-    }
+
+    const data = null
     console.log('Geocding Data',geocodingQuery.data)
+    if(geocodingQuery.data) {
+        const data = []
+        class TableObject {
+            name:string;
+            region:string;
+            country:string;
+            constructor(i:number) {
+                this.name = geocodingQuery.data.results[i].name
+                this.region = geocodingQuery.data.results[i].admin3;
+                this.country = geocodingQuery.data.results[i].country_code;
+            }
+        }
+        console.log('Starting')
+        for(let i = 0;i<geocodingQuery.data.results.length;i++) {
+            data.push(new TableObject(i))
+            console.log('datenb',data)
+        }
+        console.log(geocodingQuery.data.results[3].admin3,'Trest Admin')
     return (
-        <div>
-            <div className="SearchLocBox">{location}</div>
-            <div className="ResultNameBox"> {resultName}</div>
-        </div>
-        
-    ) 
+        <div className="">
+        <Table className="mt-8">
+          <TableHead>
+            <TableRow className="border-b border-tremor-border dark:border-dark-tremor-border">
+              <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                Name
+              </TableHeaderCell>
+              <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                Region
+              </TableHeaderCell>
+              <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                Country
+              </TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((item) => (
+              <TableRow key={item.name}>
+                <TableCell className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                  {item.name}
+                </TableCell>
+                <TableCell>{item.region}</TableCell>
+                <TableCell>{item.country}</TableCell>
+                <TableCell><button className="test" type="button" onClick={() => {console.log('Clicked')}}></button>sdfs</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    )
+    }
 }
