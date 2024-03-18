104fd6259e54d42bb121994b0f6cfb72bec586db
white <white.privat@gmail.com>
2024-03-18 20:47:49 +0100

    done some design and refactroing to geocodong but internet connection got to slow

--- src/components/Geocoding.tsx
+++ src/components/Geocoding.tsx
@@ -3,13 +3,14 @@ import { useQuery } from "@tanstack/react-query"
 import {  Table,  TableBody,  TableCell,  TableHead,  TableHeaderCell,  TableRow,} from '@tremor/react';
 import { motion } from "framer-motion";
 import Loader from "./Loader";
+import { JsxChild } from 'typescript';
 export default () => { //main func
   //var def
   const [location, setLocation] = useState('')
   const inputRef = useRef<string>();
   const [startDivY, setStartDivY] = useState<number>(-100);
   const [startDivOpacity, setStartDivOpacity] = useState<number>(0);
-
+  const [table, setTable] = useState<any>();
   //geocoding Query
   const geocodingQuery = useQuery({
         queryFn: () =>
@@ -48,87 +49,68 @@ export default () => { //main func
             for(let i = 0;i<geocodingQuery.data.results.length;i++) {
               data.push(new TableObject(i)) //pushting data from Query to data array
             }
-            return (
-              <>
-              <form onSubmit={(event) => {
-                event.preventDefault();
-                setLocation(inputRef.current)
-               }}>
-                <motion.input whileFocus={{scale:0.9}} type="text" onChange={(event) => {inputRef.current = event.target.value;}}
-                  className='fixed bottom-0 left-0 mb-2 w-2/3 z-10'
-                />
-                <motion.button whileTap={{scale:0.8}} whileHover={{scale:1.01}} type='submit' className='fixed bottom-2 right-0 mb-0 mt-0 ml-20 w-1/4 bg-dark-blue z-10'>Submit</motion.button>
-              </form>
-              <div className='overflow-auto'>
-              <motion.div initial={{opacity:0,y:500}} animate={{opacity:1,y:0}} transition={{duration:0.8}} className="h-1/3 w-1/2 absolute flex items-center justify-center bg-normal-grey rounded-3xl overflow-auto w-full h-full top-0 z-0">
-              <Table className="mt-8">
-                <TableHead>
-                  <TableRow className="border-b border-tremor-border dark:border-dark-tremor-border">
-                    <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                      Name
-                    </TableHeaderCell>
-                    <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                      Region
-                    </TableHeaderCell>
-                    <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                      Country
-                    </TableHeaderCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.map((item) => ( //map over data with each cell names 'item'
-                    <TableRow key={item.name}>
-                      <TableCell className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                        {item.name}
-                      </TableCell>
-                      <TableCell>{item.region}</TableCell>
-                      <TableCell>{item.country}</TableCell>
-                      <motion.button className="geocodingAddButton" onClick={() => { //onClick Event
-                          window.localStorage.setItem('longitude',item.longitude)
-                          window.localStorage.setItem('latitude',item.latitude)
-                          window.localStorage.setItem('location',item.name+', '+item.region)
-                          document.location.reload(); //reload dom
-                      }}
-                      whileHover={{ scale: 1.2, rotate: 90 }}
-                      whileTap={{
-                        scale: 0.8,
-                        rotate: -90,
-                        borderRadius: "100%"
-                      }}
-                      >add</motion.button>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-              <br/>
-              <br/>
-              <br/>
-            </motion.div>
-            </div>
-            </>
-          )
+            setTable(
+            <Table className="mt-8">
+            <TableHead>
+              <TableRow className="border-b border-tremor-border dark:border-dark-tremor-border">
+                <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                  Name
+                </TableHeaderCell>
+                <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                  Region
+                </TableHeaderCell>
+                <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                  Country
+                </TableHeaderCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((item) => ( //map over data with each cell names 'item'
+                <TableRow key={item.name}>
+                  <TableCell className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                    {item.name}
+                  </TableCell>
+                  <TableCell>{item.region}</TableCell>
+                  <TableCell>{item.country}</TableCell>
+                  <motion.button className="geocodingAddButton" onClick={() => { //onClick Event
+                      window.localStorage.setItem('longitude',item.longitude)
+                      window.localStorage.setItem('latitude',item.latitude)
+                      window.localStorage.setItem('location',item.name+', '+item.region)
+                      document.location.reload(); //reload dom
+                  }}
+                  whileHover={{ scale: 1.2, rotate: 90 }}
+                  whileTap={{
+                    scale: 0.8,
+                    rotate: -90,
+                    borderRadius: "100%"
+                  }}
+                  >add</motion.button>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>)
         }
-        else { //no result case
-          return (
-            <div>
-              <div className=''>
-                <motion.div className='flex items-center justify-center'>
-                  <motion.div  initial={{opacity:0,y: -100}} animate={{opacity: startDivOpacity, y:startDivY}} transition={{duration:0.8}} className='h-1/3 w-1/2 bg-dark-blue rounded-3xl text-white fixed flex items-center justify-center'>
-                  </motion.div>
-                </motion.div>
-              </div>
-              <form onSubmit={(event) => {
-                event.preventDefault();
-                setLocation(inputRef.current)
-               }}>
-                <motion.input whileFocus={{scale:0.9}} type="text" onChange={(event) => {inputRef.current = event.target.value;}} onFocus={() => {setStartDivOpacity(1);setStartDivY(0)}} onBlur={() => {setStartDivOpacity(0);setStartDivY(-100)}} 
-                  className='fixed bottom-0 left-0 mb-2 w-2/3 z-10'
-                />
-                <motion.button whileTap={{scale:0.8}} whileHover={{scale:1.01}} type='submit' className='fixed bottom-2 right-0 mb-0 mt-0 ml-20 w-1/4 bg-dark-blue z-10'>Submit</motion.button>
-              </form>
-            </div>
+         return (
+          <>
+          <form onSubmit={(event) => {
+            event.preventDefault();
+            setLocation(inputRef.current)
+           }}>
+            <motion.input whileFocus={{scale:0.9}} type="text" onChange={(event) => {inputRef.current = event.target.value;}}
+              className='fixed bottom-0 left-0 mb-2 w-2/3 z-10'
+            />
+            <motion.button whileTap={{scale:0.8}} whileHover={{scale:1.01}} type='submit' className='fixed bottom-2 right-0 mb-0 mt-0 ml-20 w-1/4 bg-dark-blue z-10'>Submit</motion.button>
+          </form>
+          <div className='flex items-center overflow-auto'>
+          <motion.div initial={{opacity:1}} animate={{height:'100%',width:'100%'}} transition={{duration:0.8}} className="md:h-1/3 md:w-1/2 h-full w-full bg-dark-blue rounded-3xl text-white flex items-center justify-center z-0">
+           {table}
+          <br/>
+          <br/>
+          <br/>
+        </motion.div>
+        </div>
+        </>
 
           )
         }
     }
-}
