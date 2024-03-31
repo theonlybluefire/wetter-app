754a4280dc6f591844b63c5617d94f7b0b8fb601
white <white.privat@gmail.com>
2024-03-31 17:11:39 +0200

    done design to geocoding (finished) + tried new approach for data loading
    map over the results array and return a div with the parameters and design, but I do not know how I could implement it into the main DOM without triggering the "Rendered more hooks than before" error

--- src/components/Geocoding.tsx
+++ src/components/Geocoding.tsx
@@ -11,6 +11,8 @@ export default () => { //main func
   const [startDivY, setStartDivY] = useState<number>(-100);
   const [startDivOpacity, setStartDivOpacity] = useState<number>(0);
   const [table, setTable] = useState<any>();
+  //test
+  const [something, setSomethings] = useState<any>();
   //geocoding Query
   const geocodingQuery = useQuery({
         queryFn: () =>
@@ -28,87 +30,34 @@ export default () => { //main func
     }
     if (geocodingQuery.error) { //error case
         return <div>geocodingQuery.error</div>
-        }
+    }
     if(geocodingQuery.data) { //data case
-      let data = [] 
-      class TableObject {
-        name:string;
-        region:string;
-        country:string;
-        longitude:string;
-        latitude:string;
-        constructor(i:number) {
-          this.name = geocodingQuery.data.results[i].name
-          this.region = geocodingQuery.data.results[i].admin3;
-          this.country = geocodingQuery.data.results[i].country_code;
-           this.longitude = geocodingQuery.data.results[i].longitude;
-           this.latitude = geocodingQuery.data.results[i].latitude
-          }
-        }
-        if(geocodingQuery.data.results) { //results case inside data case
-            for(let i = 0;i<geocodingQuery.data.results.length;i++) {
-              data.push(new TableObject(i)) //pushting data from Query to data array
-            }
-            setTable(
-            <Table className="mt-8">
-            <TableHead>
-              <TableRow className="border-b border-tremor-border dark:border-dark-tremor-border">
-                <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                  Name
-                </TableHeaderCell>
-                <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                  Region
-                </TableHeaderCell>
-                <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                  Country
-                </TableHeaderCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map((item) => ( //map over data with each cell names 'item'
-                <TableRow key={item.name}>
-                  <TableCell className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                    {item.name}
-                  </TableCell>
-                  <TableCell>{item.region}</TableCell>
-                  <TableCell>{item.country}</TableCell>
-                  <motion.button className="geocodingAddButton" onClick={() => { //onClick Event
-                      window.localStorage.setItem('longitude',item.longitude)
-                      window.localStorage.setItem('latitude',item.latitude)
-                      window.localStorage.setItem('location',item.name+', '+item.region)
-                      document.location.reload(); //reload dom
-                  }}
-                  whileHover={{ scale: 1.2, rotate: 90 }}
-                  whileTap={{
-                    scale: 0.8,
-                    rotate: -90,
-                    borderRadius: "100%"
-                  }}
-                  >add</motion.button>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>)
-        }
+      if(geocodingQuery.data.results) {
+        console.log(geocodingQuery.data)
+        geocodingQuery.data.results.map((n) => {
+          console.log(n.country_code)
+      })
+
+      }
          return (
           <>
           <form onSubmit={(event) => {
             event.preventDefault();
             setLocation(inputRef.current)
            }}>
-          <div>
-            <div className='bg-rose-400 fixed bottom-5 flex w-full md:w-2/3 h-16'>
+          <div className='fixed bottom-0 right-0 left-0 md:py-4 py-1 flex md:justify-center'>
+            <div className='bg-dark-blue/40 md:w-2/3 w-full h-16 rounded-2xl flex justify-center items-center space-x-2'>
               <motion.input whileFocus={{scale:0.9}} type="text" onChange={(event) => {inputRef.current = event.target.value;}}
-                className='left-2 mb-2 w-2/3 z-10'
+                className='relative w-2/3 z-10'
               />
-              <motion.button whileTap={{scale:0.8}} whileHover={{scale:1.01}} type='submit' className='bottom right-0 mb-0 mt-0 ml-20 w-1/4 bg-dark-blue z-10 h-12'>Submit</motion.button>
+              <motion.button whileTap={{scale:0.8}} whileHover={{scale:1.01}} type='submit' className='relative w-1/4 bg-dark-blue z-10 h-12 text-smooth-white   font-bold'>Submit</motion.button>
             </div>
           </div>
           </form>
           
           <div className='flex items-center overflow-auto'>
           <motion.div initial={{opacity:1}} animate={{height:'100%',width:'100%'}} transition={{duration:0.8}} className="md:h-1/3 md:w-1/2 h-full w-full bg-dark-blue rounded-3xl text-white flex items-center justify-center z-0 h-12">
-           {table}
+           {something}
           <br/>
           <br/>
           <br/>
