9fa5805b5dd1f140c19ae1d7e9233f23693650bf
white <white.privat@gmail.com>
2024-04-18 12:40:36 +0200

    done some design stuff and tried to add a close button, some errors fixed

--- src/components/Geocoding.tsx
+++ src/components/Geocoding.tsx
@@ -32,15 +32,15 @@ export default () => {
   useEffect(() => {
     if (geocodingQuery.data && geocodingQuery.data.results && location !== prevLocationRef.current) {
       prevLocationRef.current = location;
-      classesFirstOutputDiv.current = 'h-full w-full fixed bottom-0'
+      classesFirstOutputDiv.current = 'h-full w-full fixed bottom-0 z-0'
       setResults(
         geocodingQuery.data.results.map((item) => (
-          <div className='w-full flex justify-center'>
-            <div className='w-1/2 rounded-3xl h-20 bg-rose-600'>
+          <motion.div whileBlur={{opacity:0}} className='w-3/4 flex justify-center z-0'>
+            <div className='w-1/2 rounded-3xl h-20 bg-rose-600 z-0'>
               <h1 className='text-right font-bold'>{item.admin4}</h1>
               <p className='text-right'>{item.admin3}</p>
             </div>
-          </div>
+          </motion.div>
         ))
       );
     }
@@ -62,19 +62,19 @@ export default () => {
         event.preventDefault();
         setLocation(inputRef.current || '');
       }}>
-        <div className='fixed bottom-0 right-0 left-0 md:py-4 py-1 flex md:justify-center'>
+        <div className='fixed bottom-0 right-0 left-0 md:py-4 py-1 flex md:justify-center z-50'>
           <div className='bg-dark-blue/40 md:w-2/3 w-full h-16 rounded-2xl flex justify-center items-center space-x-2'>
             <motion.input
               whileFocus={{ scale: 0.9 }}
               type="text"
               onChange={(event) => { inputRef.current = event.target.value; }}
-              className='relative w-2/3 z-10'
+              className='relative w-2/3 z-50'
             />
             <motion.button
               whileTap={{ scale: 0.8 }}
               whileHover={{ scale: 1.01 }}
               type='submit'
-              className='relative w-1/4 bg-dark-blue z-10 h-12 text-smooth-white font-bold'
+              className='relative w-1/4 bg-dark-blue z-10 h-12 text-smooth-white font-bold z-50'
             >
               Submit
             </motion.button>
@@ -87,10 +87,16 @@ export default () => {
           initial={{ opacity: 1 }}
           animate={{ height: '100%', width: '100%' }}
           transition={{ duration: 0.8 }}
-          className="overflow-auto"
+          className="overflow-auto z-0"
         >
           {results}
         </motion.div>
+        <motion.button 
+          whileTap={{scale:0.7}} onTap={() => {setResults(null)}}
+          className='fixed top-0 right-0 bg-dark-blue z-50'>
+            
+        </motion.button>
+        
       </div>
       
     </>
