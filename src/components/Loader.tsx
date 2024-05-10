e2b65c254db7082d825988f0896389bf1da0cf44
white <white.privat@gmail.com>
2024-05-10 23:58:42 +0200

    added a new loader -> loading animation done

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,24 +1,20 @@
 import { motion } from "framer-motion";
 import React from "react";
+import { cardio } from 'ldrs'
 
 export default () => {
+  cardio.register()
   return (
-    <div className="flex justify-center items-center h-full">
-    <div className='text-center'>
-      <motion.div className="h-5 w-5 rounded-md bg-blue-700"
-        initial={{rotate:0}}
-        animate={{rotate:[180,0,-180],borderRadius:[6,10,6]}}
-        transition={{
-          duration: 1,
-          repeat: Infinity,
-          ease: "easeInOut",
-          yoyo: Infinity,
-          repeatType: "reverse"  
-        }}
-      >
-      </motion.div>
+    <div className="top-0 absolute w-full h-full backdrop-blur-md">
+      <div className="rounded-3xl absolute w-1/3 h-1/3 top-1/3 left-1/3 bg-dark-blue/30 flex grid justify-center items-center">
+        <l-cardio
+          size="50"
+          stroke="4"
+          speed="2" 
+          color="#459FF6" 
+        ></l-cardio>
+      </div> 
     </div>
-  </div>
-  );
-};
-
+    
+  )
+}
