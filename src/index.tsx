9e9afb3bcfcc6528f859a8434fc4ed75d246b6d9
white <white.privat@gmail.com>
2024-05-22 22:46:13 +0200

    forgot this

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
